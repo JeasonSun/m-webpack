aa0cd9bd86a85fc61c46210ed1f346822e55fae7
mojie <jeason8023@gmail.com>
2022-12-07 15:53:58 +0800

    feat: utils

--- webpack/utils.js
+++ webpack/utils.js
@@ -1,8 +1,20 @@
 const path = require("path");
+const fs = require("fs");
 function toUnixPath(filePath) {
   return filePath.replace(/\\/g, path.posix.sep);
 }
 
+function tryExtensions(modulePath, extensions) {
+  // console.log(modulePath, extensions)
+  for (let i = 0; i < extensions.length; i++) {
+    if (fs.existsSync(modulePath + extensions[i])) {
+      return modulePath + extensions[i];
+    }
+  }
+  // throw new Error('Module not Found ')
+}
+
 module.exports = {
   toUnixPath,
+  tryExtensions,
 };
